83c6e0217272b4d49d0022d33c45c501d16bbc5d
yrmuriel820 <yrmuriel820@gmail.com>
2016-11-28 14:12:16 -0500

    investment

--- ym1495_1007/ym1495_assignment8/investments.py
+++ ym1495_1007/ym1495_assignment8/investments.py
@@ -5,7 +5,7 @@ Created on Nov 27, 2016
 '''
 import numpy as np
 import exceptions
-from exceptions import input_bound_exception
+from exceptions import input_bound_exception,input_value_exception,input_trial_number_exception
 
 class investments(object):
     '''
@@ -18,5 +18,18 @@ class investments(object):
         Constructor
         '''
         if positions_input[0]!='[' or positions_input[-1]!=']':
-            raise input_bound_exception
+            raise input_bound_exception()
+        else:
+            positions = positions_input[1:-1].split(',')
+            for i in range(len(positions)):
+                try:
+                    positions[i] = int(positions[i])
+                except:
+                    raise input_value_exception()
+        self.positions = np.array(positions)
+        self.position_values = np.floor(1000/self.positions)
+        try:
+            self.num_trials = int(num_trials_input)
+        except:
+            raise input_trial_number_exception()
         
