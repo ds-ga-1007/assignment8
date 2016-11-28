8c2ab850bdd09d4e30af6c428ced4cf8e98c98bc
ys2506 <ys2506@nyu.edu>
2016-11-28 14:50:19 -0500

    Update exceptions.py

--- ys2506/exceptions.py
+++ ys2506/exceptions.py
@@ -1,3 +1,9 @@
+
+"""
+Created on Mon Nov 28 2016
+@author: Yanan Shi/ys2506
+@desc: handle exceptions
+"""
 #User defined exception(s) are employed for indicating error conditions
 class InvalidInputException(Exception):
     def __str__(self):
