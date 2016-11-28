1afbdc5db77102ea1a448dd88df0bf50ae44980e
Zhaopeng <zl1732@nyu.edu>
2016-11-28 00:32:17 -0500

    assignment8(modified)

--- zl1732/investment.py
+++ zl1732/investment.py
@@ -64,4 +64,3 @@ class investment:
             f.write('Position: {0}\n'.format(position))
             f.write('mean: {0}'.format(ret_mean))
             f.write('   Std {0}\n\n'.format(ret_std))
-        f.close()
