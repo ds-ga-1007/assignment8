52c67bf388939bd7c093406b5ebc9bf1db84963b
zl1271 <zl1271@nyu.edu>
2016-11-28 18:20:52 -0500

    most basic version

--- zl1271/assignment8.py
+++ zl1271/assignment8.py
@@ -3,6 +3,34 @@ Created on Nov 23, 2016
 
 @author: felix
 '''
+from simu_funcs import *
+from simulation import *
+    
+def main():
+    while True:
+        try:
+            shares_input_str = ''
+            trials_input_str = ''
+            
+            while not valid_share_str(shares_input_str):
+                shares_input_str = input("Please enter a list of 4 integers separated by commas, as number of shares to buy in parallel\n")
+                shares_input_str = rm_ws(shares_input_str)
+
+            while not valid_trial_str(trials_input_str):
+                trials_input_str = input("Please enter the number of simulation trials\n")
+                trials_input_str = rm_ws(trials_input_str)
+
+            positions = [int(string) for string in shares_input_str.split(",")]
+            num_trials = int(trials_input_str)
+            this_simu = simulation(positions,num_trials)
+            
+            this_simu.simulate()
+            break
+        except:
+            pass
 
 if __name__ == '__main__':
-    pass
+    try:
+        main()
+    except EOFError:
+        pass
---
+++ zl1271/simulation.py
@@ -0,0 +1,57 @@
+'''
+Created on Nov 27, 2016
+
+@author: felix
+'''
+import numpy as np
+from simu_funcs import get_invest_return_vec
+
+class simulation(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, positions, num_trials, money_to_invest = 1000):
+        '''
+        Constructor
+        A simulation is defined by positions and number of trials
+        '''
+        self.positions = positions
+        self.num_trials = num_trials
+        self.money_to_invest = money_to_invest
+    
+    def get_position_values(self):
+        self.values = [(self.money_to_invest / position) for position in self.positions]
+    
+    def simulate(self):
+        self.get_position_values()
+        self.simulate_results = np.zeros([self.num_trials,len(self.values)])
+        
+        print(self.values,self.positions,self.num_trials) #################################
+        
+        for trial in np.arange(self.num_trials):
+            ii = 0
+
+            for position_value in self.values:
+                
+                this_return = get_invest_return_vec(position_value,self.positions[ii])
+                this_result = self.money_to_invest + np.sum(this_return)
+                
+            
+                self.simulate_results[trial,ii] = this_result
+                ii = ii + 1
+        
+
+        print(self.simulate_results)
+        
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
